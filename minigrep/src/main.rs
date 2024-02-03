8107f809ee26d50b7a054725b4a3e4bce2f2f351
leo <leo233.huang@gmail.com>
2024-02-03 22:03:42 +0800

    学会使用结构体,使得代码更易维护

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,14 +1,24 @@
 use std::env;
 use std::fs;
 
+struct Config {
+    query: String,
+    file_path: String
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+    Config {query, file_path}
+}
+
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     //dbg!(args);
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = parse_config(&args);
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
     println!("get contents:\n{contents}");
-
 }
