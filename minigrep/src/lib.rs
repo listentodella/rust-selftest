80ab068fe4a2d533643391013d88f38555857ef0
leo233 <leo233.huang@gmail.com>
2024-02-04 21:12:43 +0800

    添加测试代码雏形

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -22,3 +22,23 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     println!("get contents:\n{contents}");
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+            Rust:
+            safe, fast, productive.
+            Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
