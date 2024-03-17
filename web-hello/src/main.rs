30180f0e7d11aa6d4e8bfd7f5bb2047fdd1f873c
leo <leo233.huang@gmail.com>
2024-03-17 13:59:19 +0800

    优化代码可读性

--- web-hello/src/main.rs
+++ web-hello/src/main.rs
@@ -45,19 +45,15 @@ fn handle_connection(mut stream:TcpStream) {
     //第一个unwrap负责处理Option，并在迭代器没有项时停止程序
     //第二个unwrap负责处理Result
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let length = contents.len();
-        let response = format!("{status_line}\r\nContents-Length: {length}\r\n\r\n{contents}");
 
-        stream.write_all(response.as_bytes()).unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
     } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404.html").unwrap();
-        let length = contents.len();
-        let response = format!("{status_line}\r\nContents-Length: {length}\r\n\r\n{contents}");
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
 
-        stream.write_all(response.as_bytes()).unwrap();
-    }
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContents-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
