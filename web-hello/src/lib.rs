e0d41b28fcde5f958136aca9b2b148fb2dda165b
leo <leo233.huang@gmail.com>
2024-03-17 16:00:05 +0800

    使用mpsc向线程发送请求

--- web-hello/src/lib.rs
+++ web-hello/src/lib.rs
@@ -1,8 +1,13 @@
+use std::sync::{Arc, mpsc, Mutex};
 use std::thread;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+// 来存放用于向信道中发送的闭包
+struct Job;
 
 impl ThreadPool {
     /// 创建线程池
@@ -14,20 +19,24 @@ impl ThreadPool {
     /// `new`函数在size为0时会panic
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         //with_capacity与new做了同样的工作，不过他为vector预先分配空间
         //而new随着后面push元素需要重新改变大小,效率上不如with_capacity
         let mut workers = Vec::with_capacity(size);
         for i in 0..size {
-            workers.push(Worker::new(i));
+            workers.push(Worker::new(i, Arc::clone(&receiver)));
         }
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
-    where
-        F: FnOnce() + Send + 'static,
-    {
-    }
+        where
+            F: FnOnce() + Send + 'static,
+    {}
 }
 
 struct Worker {
@@ -36,10 +45,12 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
+    fn new(id: usize, receiver: Arc::<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         Worker {
             id,
-            thread: thread::spawn(|| {}),
+            thread: thread::spawn(|| {
+                receiver;
+            }),
         }
     }
 }
